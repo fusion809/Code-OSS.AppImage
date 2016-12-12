00ff15f8c994dfc6ce950521217693b834e7611f
Brenton Horne <brentonhorne77@gmail.com>
2016-12-13 04:22:53 +1000

    Fixing VERSION in download-latest

--- download-latest.sh
+++ download-latest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=$(wget -q https://github.com/fusion809/Code-OSS.AppImage/releases/tag/continuous -O - | grep "x86_64.AppImage<" | cut -d '-' -f 2 | tail -n 1)
+VERSION=$(wget -q https://github.com/fusion809/Code-OSS.AppImage/releases/tag/continuous -O - | grep "x86_64.AppImage<" | cut -d '-' -f 3 | tail -n 1)
 wget -c https://github.com/fusion809/Code-OSS.AppImage/releases/download/continuous/Code-OSS-${VERSION}-x86_64.AppImage
 chmod +x code-oss-${VERSION}-x86_64.AppImage
